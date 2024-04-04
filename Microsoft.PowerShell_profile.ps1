1ed3fd27e9cff0b3ee3a70dc1907f258a100c414
dukesook <devonsookhoo14@gmailcom>
2024-04-03 21:15:23 -0600

    Initial Microsoft Profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+# Location: 
+#   - $PATH 
+#   - C:\Users\devon\OneDrive\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1
+# 
+# Create Symbolic Link in PowerShell:
+#   - New-Item -ItemType SymbolicLink -Path "C:\Users\devon\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Target "C:\Users\devon\repos\dukesook\Microsoft.PowerShell_profile.ps1"
+write-host "PowerShell Profile"
